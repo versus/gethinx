55492c2df0649da72309daa3aa3eeebec99584e5
Valentyn Nastenko <versus.ua@gmail.com>
2018-01-02 00:18:20 +0200

    add rpc message

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 	"net/url"
 	"github.com/versus/gethinx/lib"
 	"github.com/versus/gethinx/middle"
+	"github.com/versus/gethinx/rpc"
+	"encoding/json"
 )
 
 var (
 	numBlocks int64 = 3644
 )
+
+
 
 
 func reverseProxy() gin.HandlerFunc {
@@ -24,6 +28,17 @@ func reverseProxy() gin.HandlerFunc {
 		if err != nil {
 			log.Print("Error parse target %s", err.Error())
 		}
+
+        var req rpc.JsonRpcMessage
+		js := `{"jsonrpc":"2.0","id":6,"method":"eth_getBlockByNumber","params":["0xe3c",false]}`
+		bytes := []byte(js)
+		err = json.Unmarshal(bytes,&req)
+		if err != nil {
+			log.Println("Error unmarshal  %s", err.Error())
+		}
+		log.Println(req.Method)
+		log.Println(string(req.Params[0]))
+
 		proxy := httputil.NewSingleHostReverseProxy(url)
 		proxy.ServeHTTP(c.Writer, c.Request)
 	}
@@ -42,3 +57,4 @@ func main()  {
 	router.POST("/", reverseProxy())
 	router.Run(":8545")
 }
+

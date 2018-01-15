169ec3872c32ea3da5c0fb685526cd7a86854d04
Valentin Nastenko <versus@nastenko@zeoalliance.com>
2018-01-15 15:57:09 +0200

    add roundrobin algorithm for target

--- scheduler/target.go
+++ scheduler/target.go
@@ -3,9 +3,11 @@ package scheduler
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/url"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type EthBlock struct {
@@ -17,12 +19,28 @@ type EthBlock struct {
 
 func GetTargetNode(backends map[string]Upstream, block int64, lastblock *EthBlock) (*url.URL, error) {
 	wBlock := block
+	roulete := make([]string, 0)
 	if block == -1 {
 		wBlock = atomic.LoadInt64(&lastblock.Dig)
 	}
-	log.Println("target node for block:", fmt.Sprintf("%v", wBlock))
 
-	srv := backends["Q!@W#E$R%T^Y"]
+	for key, srv := range backends {
+		if srv.FSM.Current() == "active" {
+			roulete = append(roulete, key)
+			if srv.Weight > 1 {
+				for i := 0; i < int(srv.Weight-1); i++ {
+					roulete = append(roulete, key)
+				}
+			}
 
+		}
+	}
+	rand.Seed(time.Now().Unix())
+	winner := rand.Int() % len(roulete)
+	srv := backends[roulete[winner]]
+
+	//srv := backends["Q!@W#E$R%T^Y"]
+
+	log.Println("target node ", srv.Target, " for block:", fmt.Sprintf("%v", wBlock))
 	return srv.GetURL()
 }
